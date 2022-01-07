97bd8e84b099d80d02af0079fcb2e7a501095eb0
Roee Oscar <roee.oscar@live.biu.ac.il>
2022-01-07 22:55:24 +0200

    update

--- linear_prog_graph_col.py
+++ linear_prog_graph_col.py
@@ -6,7 +6,7 @@ def linear_prog_min_graph_col(g):
     coloring = [-1] * n
     vertices = range(n)
     # colors = range(1, n+1)
-    colors = range(1, max([int(sum(g[i])) for i in range(0, n)])+2)
+    colors = range(1, max([int(sum(g[i])) for i in range(0, n)]) + 2)
     lp = LpProblem(name='Linear_Programming_Graph_Coloring_Problem', sense=LpMinimize)
     colors_of_vertices = LpVariable.dicts(name='vertex_and_color', indices=(vertices, colors), cat=LpBinary)
     is_color_used = LpVariable.dicts(name='color', indices=colors, cat=LpBinary)
@@ -24,8 +24,12 @@ def linear_prog_min_graph_col(g):
         for j in range(first, n):
             if g[i][j] == 1:
                 for color in colors:
-                    lp += colors_of_vertices[i][color] + colors_of_vertices[j][color] <= is_color_used[color]
+                    lp += colors_of_vertices[i][color] + colors_of_vertices[j][color] <= 1
         first += 1
+
+    for vertex in vertices:
+        for color in colors:
+            lp += colors_of_vertices[vertex][color] <= is_color_used[color]
 
     lp.solve(PULP_CBC_CMD(msg=0))
 
